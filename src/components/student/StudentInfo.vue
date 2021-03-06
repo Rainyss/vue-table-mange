e8d291be230f1c06e3bbe57be6d1d3fec5a03dee
Rainyss <1553976022@qq.com>
2021-03-06 19:45:39 +0800

    开启eslint

---
+++ src/components/student/StudentInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <StudentDetail />
+  </div>
+</template>
+
+<script>
+import StudentDetail from './StudentDetail';
+export default {
+  components: {
+    StudentDetail
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <StudentInfo />
+  </div>
+</template>
+
+<script>
+import StudentInfo from '@/components/student/StudentInfo';
+export default {
+  name: 'Home',
+  components: {
+    StudentInfo
+  }
+};
+</script>
