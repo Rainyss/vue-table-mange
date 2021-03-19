5cb12e41b46a1bd152ade8c36c263b94f3178f90
Rainyss <1553976022@qq.com>
2021-03-19 23:55:15 +0800

    更新工具函数

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,17 @@
-export const initData = () => {
+import request from '@/utils/request'
+import qs from 'qs'
 
+export function initData(url, params) {
+  return request({
+    url: url + '?' + qs.stringify(params, { indices: false }),
+    method: 'get'
+  })
 }
 
-export const download = () => {
-
+export function download(url, params) {
+  return request({
+    url: url + '?' + qs.stringify(params, { indices: false }),
+    method: 'get',
+    responseType: 'blob'
+  })
 }
